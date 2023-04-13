9811d120b0db733d206e68e3a1e27222353713d8
kudr1kk22 <kudr1kk22@gmail.com>
2023-04-13 15:05:33 +0300

    delete storyboard

--- OpenLibraryTestApp/MainModule/BooksListViewController/BooksListViewController.swift
+++ OpenLibraryTestApp/MainModule/BooksListViewController/BooksListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BooksListViewController.swift
 //  OpenLibraryTestApp
 //
 //  Created by Eugene Kudritsky on 13.04.23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BooksListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
